b4116900a758d4d52edd6924c69c575b7d8e19d0
Alan Falloon <alan.falloon@gmail.com>
2011-02-04 21:34:36 -0500

    Implement 'firstPC' with tests
    Add an implementation for the 'firstPC' method, including some
    QuickCheck properties to ensure it behaves properly.
    
    Convergence can be pretty slow for some matrices, but apparently that
    is caused by matrices where the first eigenvalues are close.

--- src/Numeric/LinearAlgebra/NIPALS.hs
+++ src/Numeric/LinearAlgebra/NIPALS.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE ScopedTypeVariables, FlexibleContexts #-}
 -- | Nonlinear Iterative Partial Least Squares
 module Numeric.LinearAlgebra.NIPALS
     ( -- * Simplified Interface
       firstPC
     ) where
 
-import Numeric.Container
+import Numeric.LinearAlgebra
 
 -- | Calculate the first principal component of a set of samples.
 --
@@ -16,8 +17,39 @@ import Numeric.Container
 -- 
 -- > let (pc,scores,residuals) = firstPC $ fromRows samples
 -- 
-firstPC :: Product t => Matrix t -> (Vector t, Vector t, Matrix t)
-firstPC = undefined
+firstPC :: Matrix Double -> (Vector Double, Vector Double, Matrix Double)
+firstPC m = (p,t,r)
+    where
+      steps = iterate refine (t0,undefined)
+      convergence = let scores = map fst steps
+                        dscores = zipWith diffScores scores $ tail scores
+                    in smooth dscores
+      (t,p) = let steps' = zip convergence $ tail steps
+                  steps'' = dropWhile (\(c,_) -> c > threshold) steps'
+              in snd $ head steps''
+      r = m `sub` (t `outer` p)
+      t0 = head $ toColumns m
+
+      refine (t,_) = (t', p')
+          where
+            p' = toUnit (trans m <> t)
+            t' = m <> p'
+
+      diffScores ta tb = let xa = ta<.>ta
+                             xb = tb<.>tb
+                             x = abs $ xa - xb
+                         in (x / xb) :: Double
+
+      threshold = sqrt (fromIntegral (cols m + rows m)) * eps
+
+toUnit :: Vector Double -> Vector Double
+toUnit v = if mag <= 0.0 then dim v |> (1 : repeat 0) else scale (1/mag) v
+    where
+      mag = norm2 v
+
+smooth :: [Double] -> [Double]
+smooth (x0:xs@(x1:_)) = (2*x0+x1)/3 : smooth xs
+smooth _ = []
 
 -- | Calculate the first principal component -- calculating the
 -- samples fresh on every pass.
--- test/tests.hs
+++ test/tests.hs
@@ -10,21 +10,35 @@ main = defaultMain tests
 
 tests =
     [ testGroup "Accuracy"
-      [ testProperty "sameAsSVD" prop_sameAsSVD
+      [ testProperty "sameFirstPCAsSVD" prop_sameFirstPCAsSVD
+      ]
+    , testGroup "Correctness"
+      [ testProperty "resultCanRecoverInput" prop_recoverInput
       ]
     ]
 
 
-prop_sameAsSVD m = diff <= eps'
+prop_sameFirstPCAsSVD m = relErr <= svdThreshold
     where
-      eps' = eps * fromIntegral (rows m * cols m)
-      diff = norm2 $ flatten $ expPC - actPC
-      expPC :: Matrix Double
-      (expPC,_) = leftSV m
-      actPC = fromColumns actPClist
-      actPClist = take (rows m) $ unfoldr getPCs m
-      getPCs m' = let (pc,_,m'') = firstPC m'
-                  in Just (pc,m'')
+      relErrP = relativeDifference expPC actPC
+      relErrN = relativeDifference expPC (-actPC)
+      relErr = min relErrP relErrN
+      expPC = head $ toColumns lSV
+      (lSV,_) = leftSV $ trans m
+      (actPC,_,_) = firstPC m
+
+      svdThreshold = 10 * sqrt (fromIntegral (rows m * cols m) * eps)
+
+prop_recoverInput m = relErr <= eps
+    where
+      (p,t,r) = firstPC m
+      m' = (t `outer` p) `add` r
+      relErr = relativeDifference m m'
+
+relativeDifference :: (Normed c t, Container c t) => c t -> c t -> Double
+relativeDifference x y = realToFrac (norm (x `sub` y) / (norm x + norm y))
+    where norm = pnorm Infinity
+
 
 instance (Arbitrary t, Storable t) => Arbitrary (Matrix t) where
     arbitrary = sized $ \n -> do
